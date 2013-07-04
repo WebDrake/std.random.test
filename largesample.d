0bee17bdaf8f3739aee889c9faf64a720be8f463
Joseph Rushton Wakeling <joe@webdrake.net>
2013-07-04 18:17:32 +0200

    Test for sampling from larger sources.

---
+++ largesample.d
@@ -0,0 +1,19 @@
+import std.random, std.stdio;
+import test.sample;
+
+void main()
+{
+    writeln("Let's test sampling from a larger set.");
+    writeln;
+
+    sampleTest(100, 5, 1_000_000, true);
+    writeln;
+    foreach (Rng; PseudoRngTypes)
+    {
+        static if(!is(Rng == Xorshift32))
+        {
+            sampleTest!Rng(100, 5, 1_000_000, true);
+        }
+        writeln;
+    }
+}
--- smallsample.d
+++ smallsample.d
@@ -3,7 +3,7 @@ import test.sample;
 
 void main()
 {
-    writeln("Let's test the random sample functionality with small samples.");
+    writeln("Let's test sampling from a small set.");
     writeln;
 
     sampleTest(10, 5, 1_000_000, true);
@@ -13,16 +13,4 @@ void main()
         sampleTest!Rng(10, 5, 1_000_000, true);
         writeln;
     }
-
-/*    writeln("Now with bigger samples.");
-
-    writeln("Test 0.");
-    sampleTest(100, 5, 1_000_000, true);
-    i = 1;
-    foreach(Rng; PseudoRngTypes)
-    {
-        writeln("Test ", i, ".");
-        ++i;
-        sampleTest!Rng(100, 5, 1_000_000, true);
-    }*/
 }
